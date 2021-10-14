9abd8adc3b8886949e3b09b55d1d65e25242ae44
Dongyeon1201 <dongyeon1201@gmail.com>
2021-10-14 21:45:34 +0900

    edit coin_auto

--- src/coin_auto.py
+++ src/coin_auto.py
@@ -99,24 +99,25 @@ while True:
             
             # 매도 가능한 수량 확인
             order_volume = upbitUtil.getCanSellVolume(MYCOIN.market_name)
+            
+            if order_volume > 0:
+                # 주문을 위한 헤더 설정
+                headers = upbitUtil.getHeaders(query={'market': MYCOIN.market_name})
 
-            # 주문을 위한 헤더 설정
-            headers = upbitUtil.getHeaders(query={'market': MYCOIN.market_name})
+                # 코인 판매
+                upbitUtil.orderCoin(MYCOIN.market_name, SELL, order_volume, MYCOIN.current_price, headers)
 
-            # 코인 판매
-            upbitUtil.orderCoin(MYCOIN.market_name, SELL, order_volume, MYCOIN.current_price, headers)
+                # down 모드 설정
+                MYCOIN.setCoinMode("down")
 
-            # down 모드 설정
-            MYCOIN.setCoinMode("down")
+                # 코인 보유 여부 False로 변경
+                MYCOIN.setisCoinHold(False)
 
-            # 코인 보유 여부 False로 변경
-            MYCOIN.setisCoinHold(False)
+                # 기준 가격 다시 설정
+                MYCOIN.setCheckLinePrice(MYCOIN.current_price)
 
-            # 기준 가격 다시 설정
-            MYCOIN.setCheckLinePrice(MYCOIN.current_price)
-
-            # 로그 프린트
-            logging.info("\t[-] ALL 매도 & {:,} 가격으로 ALL 매수 기준 설정 [ 코인 이름 : {} / Mode : {} / isHold : {} ]".format(MYCOIN.up_line_price, MYCOIN.market_name, MYCOIN.coin_mode, MYCOIN.is_coin_hold))
+                # 로그 프린트
+                logging.info("\t[-] ALL 매도 & {:,} 가격으로 ALL 매수 기준 설정 [ 코인 이름 : {} / Mode : {} / isHold : {} ]".format(MYCOIN.up_line_price, MYCOIN.market_name, MYCOIN.coin_mode, MYCOIN.is_coin_hold))
 
         # 상승 기준에 충족할 때 -> up 모드
         elif MYCOIN.current_price > MYCOIN.check_line_price:
@@ -161,23 +162,24 @@ while True:
             current_krw = upbitUtil.getCurrentKRW(MYCOIN.coin_proportion)
             order_volume = upbitUtil.getCanBuyVolume(MYCOIN.market_name, MYCOIN.current_price, current_krw)
 
-            # 주문을 위한 헤더 설정
-            headers = upbitUtil.getHeaders(query={'market': MYCOIN.market_name})
+            if order_volume > 0:
+                # 주문을 위한 헤더 설정
+                headers = upbitUtil.getHeaders(query={'market': MYCOIN.market_name})
 
-            # 코인 구입
-            upbitUtil.orderCoin(MYCOIN.market_name, BUY, order_volume, MYCOIN.current_price, headers)
+                # 코인 구입
+                upbitUtil.orderCoin(MYCOIN.market_name, BUY, order_volume, MYCOIN.current_price, headers)
 
-            # up 모드로 설정
-            MYCOIN.setCoinMode("up")
+                # up 모드로 설정
+                MYCOIN.setCoinMode("up")
 
-            # 코인 보유 여부 True로 변경
-            MYCOIN.setisCoinHold(True)
+                # 코인 보유 여부 True로 변경
+                MYCOIN.setisCoinHold(True)
 
-            # 기준 가격 다시 설정
-            MYCOIN.setCheckLinePrice(MYCOIN.current_price)
+                # 기준 가격 다시 설정
+                MYCOIN.setCheckLinePrice(MYCOIN.current_price)
 
-            # 로그 프린트
-            logging.info("\t[-]{:,} 가격으로 ALL 매도 설정 [ 코인 이름 : {} / Mode : {} / isHold : {} ]".format(MYCOIN.down_line_price, MYCOIN.market_name, MYCOIN.coin_mode, MYCOIN.is_coin_hold))
+                # 로그 프린트
+                logging.info("\t[-]{:,} 가격으로 ALL 매도 설정 [ 코인 이름 : {} / Mode : {} / isHold : {} ]".format(MYCOIN.down_line_price, MYCOIN.market_name, MYCOIN.coin_mode, MYCOIN.is_coin_hold))
 
         # 그 외 -> pass 모드
         else:
