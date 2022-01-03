1adf5af3f06d9f67eb443714a3ff38a3e15ff982
Dongyeon1201 <dongyeon1201@gmail.com>
2022-01-03 13:28:58 +0900

    edit buy and sell filter

--- src/coin_auto.py
+++ src/coin_auto.py
@@ -180,8 +180,7 @@ while True:
                 ))
 
             # 손절 가격 도달 시 매도
-            if (upbitUtil.coins_info[CoinName]['opening_price'] > upbitUtil.coins_info[CoinName]['trade_price'] and \
-                upbitUtil.coins_info[CoinName]['trade_price'] < upbitUtil.coins_info[CoinName]['MA60'] * float(1-(1/100))) or \
+            if  upbitUtil.coins_info[CoinName]['trade_price'] < upbitUtil.coins_info[CoinName]['MA60'] * float(1-(0.5/100)) or \
                 upbitUtil.coins_info[CoinName]['trade_price'] < MYCOIN.high_price * float(1-(INPUT_COIN_DOWN/100)):
                 
                 # 매도 가능한 수량 확인
@@ -234,10 +233,11 @@ while True:
                 Current_MA60 = ((upbitUtil.coins_info[CoinName]['MA60'] * 59) + upbitUtil.coins_info[CoinName]['trade_price']) / 60                
 
                 # 매수 조건에 만족할 때
-                if upbitUtil.coins_info[CoinName]['trade_price'] < Current_MA60 * (1 + (INPUT_COIN_BUYRANGE/100)) and \
-                upbitUtil.coins_info[CoinName]['trade_price'] > Current_MA60 and \
+                if upbitUtil.coins_info[CoinName]['trade_price'] > Current_MA60 * (1 + (INPUT_COIN_BUYRANGE/100)) and \
+                upbitUtil.coins_info[CoinName]['trade_price'] < Current_MA60 * (1 + (1/100)) and \
                 upbitUtil.coins_info[CoinName]['opening_price'] < Current_MA60 and \
                 current_krw > 5000:
+                # upbitUtil.coins_info[CoinName]['trade_price'] > Current_MA60 and \
                     
                     # 매수 가능한 수량 확인 [지정가 매수에 사용]
                     # orderable_volume = upbitUtil.getCanBuyVolume(CoinName, upbitUtil.coins_info[CoinName]['trade_price'], current_krw)
